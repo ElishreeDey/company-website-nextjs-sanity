8096f68b2a137c4d5676beb3bee43169b735053b
ElishreeDey <elishreed@mindfiresolutions.com>
2026-07-06 20:13:47 +0530

    chore: initialize Sanity Studio with default configuration

---
+++ sanity/structure.ts
@@ -0,0 +1,7 @@
+import type {StructureResolver} from 'sanity/structure'
+
+
+export const structure: StructureResolver = (S) =>
+  S.list()
+    .title('Content')
+    .items(S.documentTypeListItems())
